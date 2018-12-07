eb5ae1b26a7c56d0a86615fb4a556125181de10c
Mark Vogel <markmvogel@gmail.com>
2018-12-07 13:26:15 -0500

    first

---
+++ day1.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+import myconstants
+
+
+def copy_public_desktop(target, source=myconstants.PUBLIC_DESKTOP):
+    src_files = os.listdir(source)
+    for file_name in src_files:
+        full_file_name = os.path.join(source, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, f"\\\\{target}\\C$\\Users\\Public\\Desktop\\")
+
+
+if __name__ == '__main__':
+    copy_public_desktop(myconstants.test2)

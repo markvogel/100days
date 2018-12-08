151df4c11a4d3dd2b435a6be695b24978c4b9e97
Mark Vogel <markmvogel@gmail.com>
2018-12-08 16:55:20 -0500

    rename constants for testing to make more sense

--- day2.py
+++ day2.py
@@ -1,4 +1,5 @@
 # this will get the mac address of a target machine and send it a magic packet to get it to wakeup
+# the wake on lan feature will need to be enabled on the target machine
 from wakeonlan import send_magic_packet
 from getmac import get_mac_address
 import myconstants
@@ -9,4 +10,4 @@ def wake_computer(host):
 
 
 if __name__ == '__main__':
-    wake_computer(myconstants.test)
+    wake_computer(myconstants.test_host)

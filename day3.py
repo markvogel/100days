c91c5cc5ecfa9503205d7c683f830e44424b2492
Mark Vogel <markmvogel@gmail.com>
2018-12-09 08:36:21 -0500

    day3

---
+++ day3.py
@@ -0,0 +1,18 @@
+import os
+import myconstants
+
+
+def powershell_command(command):
+    os.system(f'powershell {command}')
+
+
+def stop_computers(computer: str = "Error: No host entered"):
+    try:
+        powershell_command("Stop-Computer -Force -ComputerName " + computer)
+        print('shutdown ' + computer)
+    except:
+        print("Could not shutdown " + computer)
+
+
+if __name__ == '__main__':
+    stop_computers(myconstants.test_host)

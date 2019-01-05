78bdba1d75b663654c5aa13d7c29fb0078c42839
Mark Vogel <markmvogel@gmail.com>
2019-01-05 10:42:29 -0500

    .

--- day29.py
+++ day29.py
@@ -13,5 +13,22 @@ def list_ends2(c: list):
     return [c[0], c[len(c) - 1]]
 
 
+def get_number():
+    return int(input('Please enter a number: '))
+
+
+def fibonacci(number: int):
+    list1 = []
+    if number == 0:
+        list1 = [0]
+    elif number == 1:
+        list1 = [1]
+    else:
+        list1 = [1, 1]
+        for x in range(number):
+            list1.append(list1[-2] + list1[-1])
+        return list1
+
+
 if __name__ == '__main__':
-    print(list_ends(z))
+    print(fibonacci(get_number()))

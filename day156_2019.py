3fd7c9a81ea37ab5063fe32de1ecdc73fc0f3867
Mark Vogel <markmvogel@gmail.com>
2019-06-05 14:52:49 -0400

    .

---
+++ day156_2019.py
@@ -0,0 +1,14 @@
+import random
+
+
+def list_overlap_comprehensions(list1, list2):
+    return [i for i in list2 if i in set(list1)]
+
+
+if __name__ == '__main__':
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    c = [random.randint(1, 100) for i in range(20)]
+    d = [random.randint(1, 100) for i in range(20)]
+    # print(c, d)
+    print(list_overlap_comprehensions(c, d))

47cc9f07c0859f04b7891dbe5255d4a448830eff
Mark Vogel <markmvogel@gmail.com>
2019-07-01 14:32:38 -0400

    .

---
+++ day182_2019.py
@@ -0,0 +1,34 @@
+# https://www.py4e.com/html3/10-tuples
+
+
+# Exercise1
+def mail_log():
+    w_dict = {}
+    infile = open('mbox.txt')
+    # count = 0
+    for line in infile:
+        words = line.split()
+        # print 'Debug:', words
+        if len(words) == 0 and 'From' not in words:
+            continue
+        # if words[0] != 'From': continue
+        try:
+            if words[0] == 'From':
+                sender = words[1]
+                if sender not in w_dict:
+                    w_dict.setdefault(sender, 1)
+                    continue
+                if words[1] in w_dict:
+                    w_dict[sender] = w_dict[sender] + 1
+        except IndexError:
+            continue
+
+    top = max(w_dict)
+    print(f"{top}: {w_dict.get(top)}")
+    # sender_list = sorted(list(w_dict.items()), reverse=True)
+    sender_list = sorted([(v, k) for k, v in w_dict.items()], reverse=True)
+    print(sender_list)
+
+
+if __name__ == '__main__':
+    mail_log()

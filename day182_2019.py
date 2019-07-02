2812a59e1568ee3d2698cd14a3058dd72f3ec36f
Mark Vogel <markmvogel@gmail.com>
2019-07-02 15:28:05 -0400

    .

--- day182_2019.py
+++ day182_2019.py
@@ -30,5 +30,39 @@ def mail_log():
     print(sender_list)
 
 
+# Exercise2
+def mail_log2():
+    w_dict = {}
+    infile = open('mbox-short.txt')
+    # count = 0
+    for line in infile:
+        words = line.split(sep=':')
+        # print('Debug:', words)
+        if len(words) == 0 and 'From' not in words:
+            continue
+        # if words[0] != 'From': continue
+        try:
+            if words[0].startswith('From'):
+                hour = words[0][-2:]
+                try:
+                    int(hour)
+                except ValueError:
+                    continue
+                # print('Debug:', hour)
+                if hour not in w_dict:
+                    w_dict.setdefault(hour, 1)
+                    continue
+                if hour in w_dict:
+                    w_dict[hour] = w_dict[hour] + 1
+        except IndexError:
+            continue
+
+    # top = max(w_dict)
+    # print(f"{top}: {w_dict.get(top)}")
+    hour_list = sorted([(k, v) for k, v in w_dict.items()], reverse=False)
+    for each in hour_list:
+        print(each)
+
+
 if __name__ == '__main__':
-    mail_log()
+    mail_log2()

1180b802f227c7ebe07d4027739c3893312be76e
mark <markmvogel@gmail.com>
2019-07-13 06:39:38 -0400

    .

--- day194_2019.py
+++ day194_2019.py
@@ -4,6 +4,7 @@ import socket
 
 # exercise2
 def socket1(webpage="http://data.pr4e.org/romeo.txt"):
+    total_text = ""
     try:
         mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         mysock.connect((webpage.split(sep="/")[2], 80))
@@ -11,23 +12,22 @@ def socket1(webpage="http://data.pr4e.org/romeo.txt"):
         mysock.send(cmd)
 
         while True:
-            c = 0
-            data_length = 0
             data = mysock.recv(512)
             if len(data) < 1:
                 break
             text = data.decode()
-            new_text = ""
-            for i in text:
-                data_length += len(data)
-                if c < 2999:
-                    new_text += i
-            print(new_text, data_length)
-
+            total_text += text
         mysock.close()
+
     except TabError:
         print("you entered an invalid url ")
         exit()
+    printed_text = ""
+    for c, l in enumerate(total_text):
+        if c < 3000:
+            printed_text += l
+    print(printed_text)
+    print(len(total_text))
 
 
 # Code: http://www.py4e.com/code3/socket1.py
@@ -38,4 +38,4 @@ def get_page():
 
 
 if __name__ == '__main__':
-    socket1("http://data.pr4e.org/mbox.txt")
+    socket1("http://data.pr4e.org/")

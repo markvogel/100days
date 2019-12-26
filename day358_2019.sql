9070f9d44d4e5223d6aaaadf4c86796f3d50dad4
markvogel <markmvogel@gmail.com>
2019-12-26 08:32:39 -0800

    .

---
+++ day358_2019.sql
@@ -0,0 +1,18 @@
+ INSERT INTO celebs (id, name, age)
+ VALUES (1, 'Justin Bieber', 22);
+
+ INSERT INTO celebs (id, name, age)
+ VALUES (2, 'Beyonce Knowles', 33);
+
+ INSERT INTO celebs (id, name, age)
+ VALUES (3, 'Jeremy Lin', 26);
+
+ INSERT INTO celebs (id, name, age)
+ VALUES (4, 'Taylor Switft', 26);
+
+INSERT INTO celebs (id, name, age)
+VALUES
+(1, 'Justin Bieber', 22);
+(2, 'Beyonce Knowles', 33);
+(3, 'Jeremy Lin', 26);
+(4, 'Taylor Switft', 26);

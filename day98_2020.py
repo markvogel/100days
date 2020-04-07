b8a7d14269113c4197e496fe935f49cc5dd8d35e
markvogel <markmvogel@gmail.com>
2020-04-07 08:49:56 -0400

    parse output from Get-ADUser from powershell for just email address

---
+++ day98_2020.py
@@ -0,0 +1,22 @@
+import os
+from os import listdir
+
+
+def powershell_command(command):
+    os.system(f'powershell {command}')
+
+
+def get_users(location):
+    return listdir(f"//{location}fps01/Users")
+
+
+if __name__ == '__main__':
+    #     users = get_users("")
+    # for user in users:
+    #     print(user)
+    #     powershell_command(f"Get-ADUser -Identity {user} -Properties UserPrincipalName")
+    with open("98output.txt") as f:
+        dlist = f.readlines()
+    for i in dlist:
+        if i.startswith("UserPrincipalName"):
+            print(i.split()[-1])

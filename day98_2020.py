a15154818b844541bcb40ecb48f2938f881c7c72
markvogel <markmvogel@gmail.com>
2020-04-07 13:59:25 -0400

    minor changes moved to functions

--- day98_2020.py
+++ day98_2020.py
@@ -10,13 +10,22 @@ def get_users(location):
     return listdir(f"//{location}fps01/Users")
 
 
-if __name__ == '__main__':
-    #     users = get_users("")
-    # for user in users:
-    #     print(user)
-    #     powershell_command(f"Get-ADUser -Identity {user} -Properties UserPrincipalName")
-    with open("98output.txt") as f:
+def app(loc):
+    users = get_users(loc)
+    print(loc)
+    for user in users:
+        print(user)
+        powershell_command(f"Get-ADUser -Identity {user} -Properties UserPrincipalName")
+
+
+def app2():
+    with open("day98_2020_output.txt") as f:
         dlist = f.readlines()
     for i in dlist:
         if i.startswith("UserPrincipalName"):
             print(i.split()[-1])
+
+
+if __name__ == '__main__':
+    app("")
+    # app2()
